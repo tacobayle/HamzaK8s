fb190e955672cd3fc907172127c31f33cc7faeff
tacobayle <nicolas.bayle@gmail.com>
2021-09-27 16:15:32 +0000

    20210927

--- variables.tf
+++ variables.tf
@@ -74,7 +74,7 @@ variable "ansible" {
     aviPbAbsentUrl = "https://github.com/tacobayle/ansiblePbAviAbsent"
     aviPbAbsentTag = "v1.54"
     aviConfigureUrl = "https://github.com/tacobayle/aviConfigure"
-    aviConfigureTag = "v5.89"
+    aviConfigureTag = "v5.9"
     k8sInstallUrl = "https://github.com/tacobayle/ansibleK8sInstall"
     k8sInstallTag = "v1.56"
   }
